5489323ac6d7f1a3311d25944415d91167735c38
Гончаров Никита <nnikitagoncharovv@gmail.com>
2023-02-07 20:28:59 +0300

    The project is completed

---
+++ yatube/posts/models.py
@@ -0,0 +1,58 @@
+from django.contrib.auth import get_user_model
+
+from django.db import models
+
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    """Модель для групп."""
+
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Заголовок')
+    slug = models.SlugField(
+        max_length=150,
+        unique=True,
+        verbose_name='Элементы')
+    description = models.TextField(
+        max_length=400,
+        verbose_name='Описание')
+
+    class Meta:
+        verbose_name_plural = 'Группы'
+        verbose_name = 'Группа'
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    """Модель для постов"""
+
+    text = models.TextField(
+        verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор',)
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name_plural = 'Посты'
+        verbose_name = 'Пост'
+
+    def __str__(self):
+        return self.text
---
+++ yatube/posts/views.py
@@ -0,0 +1,85 @@
+from django.conf import settings
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Post, Group
+from .forms import PostForm
+
+NUMBER_OF_POSTS_PER_PAGE = 10
+
+User = get_user_model()
+
+
+def index(request):
+    posts = Post.objects.select_related('author', 'group')
+    paginator = Paginator(posts, NUMBER_OF_POSTS_PER_PAGE)
+    page_obj = paginator.get_page(request.GET.get('page'))
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.select_related('group')
+    paginator = Paginator(posts, NUMBER_OF_POSTS_PER_PAGE)
+    page_obj = paginator.get_page(request.GET.get('page'))
+    context = {
+        'group': group,
+        'page_obj': page_obj
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    post_list = Post.objects.filter(author=author)
+    paginator = Paginator(post_list, NUMBER_OF_POSTS_PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj, 'author': author
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    posts_count = Post.objects.filter(author=post.author).count()
+    context = {
+        'post': post,
+        'posts_count': posts_count,
+        'requser': request.user}
+    return render(request, 'posts/post_detail.html', context)
+
+
+@login_required
+def post_create(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        create_post = form.save(commit=False)
+        create_post.author = request.user
+        create_post.save()
+        return redirect('posts:profile', create_post.author)
+    context = {
+        'form': form
+    }
+    return render(request, 'posts/create_post.html', context)
+
+
+@login_required
+def post_edit(request, post_id):
+    edit_post = get_object_or_404(Post, id=post_id)
+    if request.user != edit_post.author:
+        return redirect('posts:post_detail', post_id)
+    form = PostForm(request.POST or None, instance=edit_post)
+    if form.is_valid():
+        form.save()
+        return redirect('posts:post_detail', post_id)
+    context = {
+        'form': form, 'is_edit': True
+    }
+    return render(request, 'posts/create_post.html', context)
