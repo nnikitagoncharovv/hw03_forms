12c5a28a2acada4acda65478d044cd9a7ab4131b
Гончаров Никита <nnikitagoncharovv@gmail.com>
2023-02-10 23:56:37 +0300

    Fixed the code style

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
